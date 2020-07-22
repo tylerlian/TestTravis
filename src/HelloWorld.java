3f8a88d5190b430fdda260fa2220d0831193c0e0
tylerlian <tylerlian@gmail.com>
2020-07-22 09:32:15 -0700

    changed smells

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,7 @@
 public class HelloWorld {
 
-	public HelloWorld() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		logger.log("Hello, World");
 	}
 
 }
